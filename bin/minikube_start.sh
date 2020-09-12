a2a2ae49c7224fb1bec934326167a212defdf337
Ori Hoch <ori@uumpa.com>
2020-09-12 10:53:06 +0300

    retry starting minikube if it fails

--- bin/minikube_start.sh
+++ bin/minikube_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-minikube start --driver=docker --kubernetes-version=v1.16.14 &
+tests/wait_for.sh "minikube start --driver=docker --kubernetes-version=v1.16.14" "240" "waited too long for minikube to start" &
