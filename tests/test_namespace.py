28f6b80810f78470830884063654ecb73772eda2
Ori Hoch <ori@uumpa.com>
2020-10-28 00:00:27 +0200

    disable the full prometheus test by default

--- tests/test_namespace.py
+++ tests/test_namespace.py
@@ -1,3 +1,4 @@
+import os
 import time
 import requests
 import subprocess
@@ -138,31 +139,32 @@ def test_metrics_check_prometheus_rate_query():
             config.DEPLOYMENT_TYPES['minio']['metrics_checks'][0]['query'],
             debug=True
         ) == 0.0
-        with init_wait_deploy_helm(namespace_name) as tmpdir:
-            print("Waiting for pods to be running...")
-            wait_for_func(lambda: subprocess.getstatusoutput('kubectl -n cwdtest get pods | grep Running | wc -l')[1], "1", 60,
-                          "waited too long for pods to be running")
-            print("Starting port forwards")
-            http = subprocess.Popen('exec kubectl -n {} port-forward deployment/minio 8080:8080'.format(namespace_name), shell=True)
-            https = subprocess.Popen('exec kubectl -n {} port-forward deployment/minio 8443:8443'.format(namespace_name), shell=True)
-            time.sleep(2)
-            try:
-                print("Generating requests over a period of 2 minutes to gather metrics")
-                for i in range(20):
-                    requests.get('http://localhost:8080/minio/login', headers={"User-Agent": "Mozilla"}, timeout=5)
-                    requests.get('https://localhost:8443/minio/login', headers={"User-Agent": "Mozilla"}, timeout=5, verify=False)
-                    time.sleep(6)
-            finally:
-                http.terminate()
-                https.terminate()
-            assert namespace.metrics_check_prometheus_rate_query(
-                namespace_name,
-                config.DEPLOYMENT_TYPES['minio']['metrics_checks'][0]['query'],
-                debug=True
-            ) > 10.0
-            print("Sleeping 2 minutes to check no value for the network metrics..")
-            time.sleep(120)
-            query = config.DEPLOYMENT_TYPES['minio']['metrics_checks'][0]['query'].replace('5m', '1m')
-            assert namespace.metrics_check_prometheus_rate_query(namespace_name, query, debug=True) == 0.0
+        if os.environ.get("FULL_PROMETHEUS_TEST") == "yes":
+            with init_wait_deploy_helm(namespace_name) as tmpdir:
+                print("Waiting for pods to be running...")
+                wait_for_func(lambda: subprocess.getstatusoutput('kubectl -n cwdtest get pods | grep Running | wc -l')[1], "1", 60,
+                              "waited too long for pods to be running")
+                print("Starting port forwards")
+                http = subprocess.Popen('exec kubectl -n {} port-forward deployment/minio 8080:8080'.format(namespace_name), shell=True)
+                https = subprocess.Popen('exec kubectl -n {} port-forward deployment/minio 8443:8443'.format(namespace_name), shell=True)
+                time.sleep(2)
+                try:
+                    print("Generating requests over a period of 2 minutes to gather metrics")
+                    for i in range(20):
+                        requests.get('http://localhost:8080/minio/login', headers={"User-Agent": "Mozilla"}, timeout=5)
+                        requests.get('https://localhost:8443/minio/login', headers={"User-Agent": "Mozilla"}, timeout=5, verify=False)
+                        time.sleep(6)
+                finally:
+                    http.terminate()
+                    https.terminate()
+                assert namespace.metrics_check_prometheus_rate_query(
+                    namespace_name,
+                    config.DEPLOYMENT_TYPES['minio']['metrics_checks'][0]['query'],
+                    debug=True
+                ) > 10.0
+                print("Sleeping 2 minutes to check no value for the network metrics..")
+                time.sleep(120)
+                query = config.DEPLOYMENT_TYPES['minio']['metrics_checks'][0]['query'].replace('5m', '1m')
+                assert namespace.metrics_check_prometheus_rate_query(namespace_name, query, debug=True) == 0.0
     finally:
         prompf.terminate()
