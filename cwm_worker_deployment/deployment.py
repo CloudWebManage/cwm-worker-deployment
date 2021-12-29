765412ee136a7f35cb14791cee0f8de3828ec3e0
Ori Hoch <ori@uumpa.com>
2021-12-29 19:04:30 +0200

    add preprocess step to deploy to support parallel deployments

--- cwm_worker_deployment/deployment.py
+++ cwm_worker_deployment/deployment.py
@@ -37,30 +37,50 @@ def init(spec, namespace_lib=None):
     namespace_lib.init(namespace_name)
 
 
+def deploy_preprocess_specs(specs, helm_lib=None):
+    if not helm_lib:
+        helm_lib = helm
+    helm_latest_versions = {}
+    preprocess_results = {}
+    for key, spec in specs.items():
+        spec = {**spec}
+        deployment_spec = spec.pop('cwm-worker-deployment')
+        deployment_type = deployment_spec["type"]
+        assert deployment_type in config.DEPLOYMENT_TYPES, 'unknown deployment type: {}'.format(deployment_type)
+        if deployment_type == 'minio':
+            spec.setdefault('minio', {})['serveSingleProtocolPerPod'] = True
+        namespace_name = deployment_spec['namespace']
+        release_name = _get_release_name(namespace_name, deployment_type)
+        chart_repo = "https://raw.githubusercontent.com/CloudWebManage/cwm-worker-helm/master/cwm-worker-deployment-{}".format(deployment_type)
+        repo_name = "cwm-worker-deployment-{}".format(deployment_type)
+        chart_name = "cwm-worker-deployment-{deployment_type}".format(deployment_type=deployment_type)
+        version = deployment_spec.get('version', 'latest')
+        chart_path = deployment_spec.get('chart-path')
+        if not chart_path:
+            if version == 'latest':
+                if deployment_type in helm_latest_versions:
+                    version = helm_latest_versions[deployment_type]
+                else:
+                    version = helm_latest_versions[deployment_type] = helm_lib.get_latest_version(
+                        "https://raw.githubusercontent.com/CloudWebManage/cwm-worker-helm/master/cwm-worker-deployment-{}".format(deployment_type),
+                        'cwm-worker-deployment-{}'.format(deployment_type)
+                    )
+            else:
+                chart_path = helm_lib.chart_cache_init(chart_name, version, chart_repo)
+        preprocess_results[key] = namespace_name, release_name, repo_name, chart_name, version, spec, chart_path, chart_repo
+    return preprocess_results
+
+
 # example timeout string: "5m0s"
-def deploy(spec, dry_run=False, atomic_timeout_string=None, with_init=True, namespace_lib=None, helm_lib=None):
+def deploy(spec, dry_run=False, atomic_timeout_string=None, with_init=True, namespace_lib=None,
+           helm_lib=None, preprocess_result=None):
     if not namespace_lib:
         namespace_lib = namespace
     if not helm_lib:
         helm_lib = helm
-    spec = {**spec}
-    deployment_spec = spec.pop('cwm-worker-deployment')
-    deployment_type = deployment_spec["type"]
-    assert deployment_type in config.DEPLOYMENT_TYPES, 'unknown deployment type: {}'.format(deployment_type)
-    if deployment_type == 'minio':
-        spec.setdefault('minio', {})['serveSingleProtocolPerPod'] = True
-    namespace_name = deployment_spec['namespace']
-    release_name = _get_release_name(namespace_name, deployment_type)
-    version = deployment_spec.get('version', 'latest')
-    chart_path = deployment_spec.get('chart-path')
-    if not chart_path and version == 'latest':
-        version = helm_lib.get_latest_version(
-            "https://raw.githubusercontent.com/CloudWebManage/cwm-worker-helm/master/cwm-worker-deployment-{}".format(deployment_type),
-            'cwm-worker-deployment-{}'.format(deployment_type)
-        )
-    chart_repo = "https://raw.githubusercontent.com/CloudWebManage/cwm-worker-helm/master/cwm-worker-deployment-{}".format(deployment_type)
-    repo_name = "cwm-worker-deployment-{}".format(deployment_type)
-    chart_name = "cwm-worker-deployment-{deployment_type}".format(deployment_type=deployment_type)
+    if preprocess_result is None:
+        preprocess_result = deploy_preprocess_specs({0: spec}, helm_lib=helm_lib)[0]
+    namespace_name, release_name, repo_name, chart_name, version, spec, chart_path, chart_repo = preprocess_result
     if with_init:
         namespace_lib.init(namespace_name, dry_run=dry_run)
     returncode, stdout, stderr = helm_lib.upgrade(
